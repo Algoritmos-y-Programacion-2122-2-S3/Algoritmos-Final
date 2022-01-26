0cbec6794d0baa3a1742b25f3ca229a563657344
Antonio Guerra <anapoleon@applaudostudios.com>
2022-01-26 09:20:52 -0400

    🐛 Fixing variable names

--- number_triangles.py
+++ number_triangles.py
@@ -1,21 +1,21 @@
 def main():
     rows_number = int(input("How many rows you want?\n"))
-    rowConter = 1
-    currentRow = 0
+    row_conter = 1
+    current_row = 0
 
-    while currentRow < rows_number:
-        if currentRow == 0:
-            print(rowConter)
+    while current_row < rows_number:
+        if current_row == 0:
+            print(row_conter)
         else:
-            aux = rowConter
+            aux = row_conter
             while aux >= 1:
                 if aux == 1:
                     print(aux)
                 else:
                     print(aux, end= " ")
                 aux -= 2
-        currentRow +=1
-        rowConter += 2
+        current_row +=1
+        row_conter += 2
 
 
 if __name__ == "__main__":
